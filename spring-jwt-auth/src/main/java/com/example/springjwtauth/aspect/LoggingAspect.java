02ce37c3525f49d915f8e82cb5bd75f53ae9b0c3
Arun A <arunalakkalpalla@gmail.com>
2025-10-26 13:54:17 +0530

    Implemented named pointcut expressions and designators

--- spring-jwt-auth/src/main/java/com/example/springjwtauth/aspect/LoggingAspect.java
+++ spring-jwt-auth/src/main/java/com/example/springjwtauth/aspect/LoggingAspect.java
@@ -1,10 +1,7 @@
 package com.example.springjwtauth.aspect;
 
 import org.aspectj.lang.ProceedingJoinPoint;
-import org.aspectj.lang.annotation.After;
-import org.aspectj.lang.annotation.Around;
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.*;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -15,6 +12,24 @@ public class LoggingAspect {
     // com.example.springjwtauth.service.StateService.*() - All methods inside StateService class
     // com.example.springjwtauth.service.*.*(..) - All methods in all service package
     // com.example.springjwtauth.*.*.*(..) - All methods in the base package
+
+
+    // Pointcut Designator
+    // execution(* com.example.springjwtauth.service.StateService.*()) - Executed for matching methods[Specifically for methods]
+    // within(com.example.springjwtauth.service.StateService) - Executed for all methods within this service[Specifically for classes]
+    // target(com.example.springjwtauth.service.StateService) - Executed when the instance of StateService is created
+    //                                                          Methods are executed
+    // @within(org.springframework.stereotype.Service) - Executed for all classes with @Service annotation
+    // @annotation(org.springframework.stereotype.Service) - Executed for all methods with @PostMapping annotation
+    // @target(org.springframework.stereotype.Service) - Executed when the instance of StateService is created
+    //                                                   Methods are executed with the given target Annotation
+
+    // Named point cut
+    @Pointcut("execution(* com.example.springjwtauth.service.StateService.*())")
+    public void pointcutExpn() {}
+
+    @Pointcut("within(com.example.springjwtauth.service.StateService)")
+    public void withInPointcutExpn() {}
 
     // Executed before the method execution
     @Before("execution(* com.example.springjwtauth.service.StateService.getAllStates(..))")
@@ -29,7 +44,7 @@ public class LoggingAspect {
     }
 
     // Executed throughout the method execution
-    @Around("execution(* com.example.springjwtauth.service.StateService.*())")
+    @Around("pointcutExpn()")
     public Object aroundLog(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
         // Executed in the starting
         System.out.println("Logging aspect is called - Method started[Around log]");
@@ -41,4 +56,15 @@ public class LoggingAspect {
         System.out.println("Logging aspect is called - Method ended[Around log]");
         return result;
     }
+
+    @AfterReturning("withInPointcutExpn()")
+    public void returningLog() {
+        System.out.println("Method execution completed - [Afterreturning log]");
+    }
+
+    @AfterThrowing("pointcutExpn()")
+    public void throwingLog() {
+        System.out.println("Method thrown error - [Afterthrowing log]");
+    }
+
 }
